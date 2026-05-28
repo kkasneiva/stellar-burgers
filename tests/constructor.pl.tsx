0e0625c826db21b387a24d0d5166f2e1d373ac01
Ksenia Gosteva <kseniiagostewa@yandex.ru>
2026-05-28 19:48:58 +0300

    Добавлен e2e тест конструктора

---
+++ tests/constructor.pl.tsx
@@ -0,0 +1,37 @@
+import { test, expect } from '@playwright/test';
+
+const bunId = '643d69a5c3f7b9001cfa093c';
+const mainId = '643d69a5c3f7b9001cfa0941';
+const sauceId = '643d69a5c3f7b9001cfa0942';
+
+test.beforeEach(async ({ page }) => {
+  await page.routeFromHAR('tests/hars/burger-api.har', {
+    url: 'https://norma.education-services.ru/api/**',
+    update: false
+  });
+
+  await page.goto('/');
+});
+
+test('adds ingredients to burger constructor', async ({ page }) => {
+  await expect(page.getByText('Соберите бургер')).toBeVisible();
+
+  await expect(page.getByTestId('constructor-empty-bun-top')).toBeVisible();
+  await expect(page.getByTestId('constructor-empty-filling')).toBeVisible();
+  await expect(page.getByTestId('constructor-empty-bun-bottom')).toBeVisible();
+
+  await page.getByTestId(`add-ingredient-${bunId}`).locator('button').click();
+  await page.getByTestId(`add-ingredient-${mainId}`).locator('button').click();
+  await page.getByTestId(`add-ingredient-${sauceId}`).locator('button').click();
+
+  const constructor = page.getByTestId('burger-constructor');
+
+  await expect(constructor).toContainText('Краторная булка N-200i (верх)');
+  await expect(constructor).toContainText('Краторная булка N-200i (низ)');
+  await expect(constructor).toContainText('Биокотлета из марсианской Магнолии');
+  await expect(constructor).toContainText('Соус Spicy-X');
+
+  await expect(page.getByTestId('constructor-empty-bun-top')).not.toBeVisible();
+  await expect(page.getByTestId('constructor-empty-filling')).not.toBeVisible();
+  await expect(page.getByTestId('constructor-empty-bun-bottom')).not.toBeVisible();
+});
